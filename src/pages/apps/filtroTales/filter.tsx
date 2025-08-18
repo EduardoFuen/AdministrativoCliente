2dbb53eac2aa065a2b755c711d766ba997a2d9af
ReneNo <elrenenoguera@gmail.com>
2025-08-18 15:49:47 -0400

    se termino la funcion para filtrar por fecha

--- src/pages/apps/filtroTales/filter.tsx
+++ src/pages/apps/filtroTales/filter.tsx
@@ -1,48 +1,131 @@
 
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import data2 from "./data2.json"
-import { useSelector } from 'react-redux';
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import React from "react";
 import { DatePicker } from '@mui/x-date-pickers';
-import { TextField } from '@mui/material';
+import { Grid, TextField } from '@mui/material';
+import {Button} from "@mui/material"
 
 
 
-const Filter = () => {
 
+interface Article {
+  id: string | number;
+  name: string;
+  quantity: number;
+  price: number;
+}
+
+interface ReceptionData {
+  ID?: string | number;
+  SupplierID?: number;
+  Supplier?: { name: string };
+  Warehouse?: object;
+  WarehouseID?: number;
+  Notes?: string;
+  Discount?: string | number;
+  DiscountEarliyPay?: number;
+  SubTotal?: number;
+  DiscountGlobal?: number;
+  SubtotalWithDiscount?: number;
+  CreatedAd?: string;
+  Total?: number;
+  Tax?: number;
+  Status?: number;
+  Articles?: Article[];
+  detailsReption?: any;
+}
+
+const filterByRange = (data: ReceptionData[], property: keyof ReceptionData, range: { min: number, max: number }): ReceptionData[] => {
+  if (!Array.isArray(data) || !range || typeof range.min !== 'number' || typeof range.max !== 'number') {
+    console.error("Los argumentos de entrada no son válidos.");
+    return [];
+  }
+
+   return data.filter(item => {
+    const value = item[property];
+
+    
+    const numericValue = typeof value === 'string' ? new Date(value).getTime() : typeof value === 'number' ? value : undefined;
+
+    if (typeof numericValue !== 'number') {
+      return false;
+    }
+
+    return numericValue >= range.min && numericValue <= range.max;
+  });
+};
+
+export default function Filter() {
+  const jsonData: ReceptionData[] = data2
+
+  
 
  const [dataForm, setDataForm] = React.useState<{
         dateFrom: Date | null,
         dateTo: Date | null,
-        selected: string | number
+        
     }>({
       dateFrom: new Date(),
       dateTo: new Date(),
-      selected: -1
+      
     })
-
-const  lista  = useSelector(() => data2);
-
-
-
-console.log (lista)
-
-
+    const [data, setData] = React.useState<ReceptionData[]>(jsonData);
+  const [min, setMin] = React.useState<number>(new Date("2024-01-01").getTime());
+  const [max, setMax] = React.useState<number>(new Date("2025-06-20").getTime());
+  React.useEffect(() => {
+    const filteredData = filterByRange(jsonData, 'CreatedAd', { min, max });
+    setData(filteredData)
+  
+  }, [min, max]);
+   console.log (data)
+console.log (min,max)
   return (
-   <LocalizationProvider dateAdapter={AdapterDateFns}>  
-    <DatePicker
+    <>
+
+<LocalizationProvider dateAdapter={AdapterDateFns}>  
+    <Grid container spacing={3}>
+  <Grid item xs={8}>
+      <DatePicker
                 disableFuture
-                label='Date From'
+                label='Desde'
                 value={dataForm.dateFrom}
+                
                 inputFormat= "dd/MM/yyyy"
                 onChange={(newValue: Date | null) => {
                   setDataForm({ ...dataForm, dateFrom: newValue })
+                  const newMin = newValue ? newValue.getTime() : 0;
+                  setMin(newMin);
                 }}
-               renderInput={(params) => <TextField {...params}  />}
+               renderInput={(params) => <TextField {...params} />}
               />
-   </LocalizationProvider>
+
+    
+  </Grid>
+  <Grid item xs={8}>
+      <DatePicker
+                disableFuture
+                label='Hasta'
+                value={dataForm.dateTo}
+                minDate={dataForm.dateFrom === null ? undefined : dataForm.dateFrom}
+                inputFormat= "dd/MM/yyyy"
+                onChange={(newValue: Date | null) => {
+                  setDataForm({ ...dataForm, dateTo: newValue })
+                  const newMax = newValue ? newValue.getTime() : 0;
+                  setMax(newMax);
+                }}
+               renderInput={(params) => <TextField {...params} />}
+              />
+  </Grid>
+  <Grid item xs = {8}>
+  <Button variant='contained' > Filtrar </Button>
+</Grid>
+</Grid>
+
+</LocalizationProvider>
+
+   </>
   )
 }
 
-export default Filter
